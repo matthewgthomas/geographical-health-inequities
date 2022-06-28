5ed7467471fe5ccb007ef3867562f60ae0ee3d08
Matt Thomas <matthewgthomas@gmail.com>
2022-06-28 17:01:25 +0100

    Plot HLE by deprivation and rurality

--- plots to save.R
+++ plots to save.R
@@ -2,6 +2,7 @@ library(tidyverse)
 
 source("regional inequalities.R")
 
+# ---- Male HLE by region and rurality ----
 hle %>%
   filter(Sex == "Male") %>% 
   filter(RGNNM %in% c("East of England", "North East")) %>% 
@@ -23,6 +24,7 @@ hle %>%
 
 ggsave("charts/male healthy life expectancy by region and rurality.png", height = 80, width = 150, units = "mm")
 
+# ---- Male HLE by region and rurality ----
 hle %>%
   filter(Sex == "Female") %>% 
   filter(RGNNM %in% c("East of England", "North East")) %>% 
@@ -36,3 +38,18 @@ hle %>%
   labs(x = "Deprivation decile (1 = most deprived)", colour = "", fill = "") +
   theme_classic() +
   theme(legend.position = "bottom")
+
+# ---- HLE by deprivation and rurality ----
+hle %>%
+  ggplot(aes(x = factor(Score_Decile), y = `Healthy life expectancy`, colour = RUC)) +
+  geom_violin(draw_quantiles = 0.5) +
+  
+  facet_wrap(~Sex) +
+  
+  scale_color_manual(values = c("#a6611a", "#018571")) + # Colours from ColorBrewer
+  # scale_fill_manual(values = c("#dfc27d", "#80cdc1")) +
+  labs(x = "Deprivation decile (1 = most deprived)", colour = "", fill = "") +
+  theme_classic() +
+  theme(legend.position = "bottom")
+
+ggsave("charts/healthy life expectancy by deprivation and rurality.png", height = 80, width = 150, units = "mm")
